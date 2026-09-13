1afd29b9e437ab6b0b923226a63bf4aa0c0fc4fe
Abhishek Jain <48586213+R3-b0ot@users.noreply.github.com>
2026-09-14 00:53:23 +0530

    chore: configure plugin bundling

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['@nuclearplayer/plugin-sdk'],
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
